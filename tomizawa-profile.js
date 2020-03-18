dc2acdd68cf0b003388b82b21ecda91b572b5e84
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-18 09:34:56 +0900

    tomizawa㉜

---
+++ tomizawa-profile.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(window).scroll(function() {
+    $('.inner2').each(function() {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 70) {
+        $(this).addClass('active');
+      }
+    });
+  });
+});
