03ee3f3780b7ffd3ebdb1b4f0c691c3575918502
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-03-28 14:10:49 +0900

    ES6への置き換え

--- tomizawa-profile.js
+++ tomizawa-profile.js
@@ -1,18 +1,20 @@
-document.getElementById('accordion_menu').addEventListener('click', function () {
+'use strict';
+
+document.getElementById('accordion_menu').addEventListener('click', () => {
   document.getElementById('navlist').classList.toggle('open');
 });
 
-window.addEventListener("DOMContentLoaded", function() {
-  window.onscroll = function (){
-    var myFade = document.getElementsByClassName("inner2");
-    for(var i = 0; i < myFade.length; i++){
-      var targetElement = myFade[i].getBoundingClientRect(); //ターゲット要素の高さ
-      var scroll = document.documentElement.scrollTop || document.body.scrollTop;  //スクロール
-      var windowHeight = window.innerHeight;  //ウィンドウの高さ
-      if (scroll > scroll + targetElement.top - windowHeight + 70){
-        myFade[i].style.opacity = "1";
-        myFade[i].style.transform = "translateY(0)";
+window.addEventListener('DOMContentLoaded', () => {
+  window.onscroll = () => {
+    const myFade = document.getElementsByClassName('inner2');
+    for(let i = 0; i < myFade.length; i++) {
+      const targetElement = myFade[i].getBoundingClientRect(); //ターゲット要素の高さ
+      const scroll = document.documentElement.scrollTop || document.body.scrollTop;  //スクロール
+      const windowHeight = window.innerHeight;  //ウィンドウの高さ
+      if (scroll > scroll + targetElement.top - windowHeight + 70) {
+        myFade[i].style.opacity = '1';
+        myFade[i].style.transform = 'translateY(0)';
       }
     }
   }
-}, false);
+});
