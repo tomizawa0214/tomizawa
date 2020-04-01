fd71b977237d3442e139552a72b4465f4bada5ea
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-01 15:38:43 +0900

    Service

--- tomizawa-top/tomizawa-top.js
+++ tomizawa-top/tomizawa-top.js
@@ -1,12 +1,12 @@
-$(function() {
-  $(window).scroll(function() {
-    $('.service, .service-message, .section1, .section2, .section3, .works, .works-message').each(function() {
-      var position = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
-      if (scroll > position - windowHeight + 70) {
-        $(this).addClass('active');
-      }
-    });
-  });
-});
+// $(function() {
+//   $(window).scroll(function() {
+//     $('.service, .service-message, .section1, .section2, .section3, .works, .works-message').each(function() {
+//       var position = $(this).offset().top;
+//       var scroll = $(window).scrollTop();
+//       var windowHeight = $(window).height();
+//       if (scroll > position - windowHeight + 70) {
+//         $(this).addClass('active');
+//       }
+//     });
+//   });
+// });
