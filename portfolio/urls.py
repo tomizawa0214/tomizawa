f5f8c2f919cb06991ba18424634d2aa6e9af6ffa
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-05 17:25:27 +0900

    JS追加

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import IndexView, ContactFormView, ContactResultView
+from .views import IndexView, ContactFormView, ContactThanksView
 
 urlpatterns = [
   path('', IndexView.as_view(), name='index'),
   path('contact/', ContactFormView.as_view(), name='contact_form'),
-  path('contact/result/', ContactResultView.as_view(), name='contact_result'),
+  path('contact/thanks/', ContactThanksView.as_view(), name='contact_thanks'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -9,16 +9,17 @@ class IndexView(TemplateView):
   template_name = 'portfolio/index.html'
 
 class ContactFormView(FormView):
-  template_name = 'contact/contact_form.html'
+  template_name = 'portfolio/contact_form.html'
+  redirect_field_name = 'portfolio/contact_thanks.html'
   form_class = ContactForm
-  success_url = reverse_lazy('contact_result')
+  # success_url = reverse_lazy('contact_thanks')
 
   def form_valid(self, form):
     form.send_email()
     return super().form_valid(form)
 
-class ContactResultView(TemplateView):
-  template_name = 'contact/contact_result.html'
+class ContactThanksView(TemplateView):
+  template_name = 'portfolio/contact_thanks.html'
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
