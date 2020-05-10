276700357a1a32059ad42066848665953b000280
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-05-11 08:47:11 +0900

    環境変数設定

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,16 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$e%(==pkb5ykui)d^^2w%1^vuc67h-b)59$al0_xx$3huzj*wc'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'j.tomizawa.japan@gmail.com'
-EMAIL_HOST_PASSWORD = 'bhbwwkqqizjeeojy'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -126,6 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 django_heroku.settings(locals())
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+# Debug=Falseの時だけ実行する設定
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
+    SECRET_KEY = os.environ['SECRET_KEY']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
