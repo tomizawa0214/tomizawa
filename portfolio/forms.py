5e97d434f80f149cda268f0e3edfe318b88afa54
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-08-14 17:38:57 +0900

    お問い合わせ変更

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,24 +1,6 @@
 from django import forms
-from django.conf import settings
-from django.core.mail import BadHeaderError, send_mail, EmailMessage
-from django.http import HttpResponse
 
 class ContactForm(forms.Form):
-  name = forms.CharField (max_length = 100)
-  email = forms.EmailField ()
+  name = forms.CharField (max_length=30)
+  email = forms.EmailField (min_length=7, max_length=256)
   message = forms.CharField (widget=forms.Textarea)
-
-  def send_email(self):
-    name = self.cleaned_data['name']
-    email = self.cleaned_data['email']
-    message = self.cleaned_data['message']
-    subject = "お問い合わせありがとうございます"
-    message = '※このメールはシステムからの自動返信です。\n\n{0}様\n\nお問い合わせいただき、ありがとうございます。\n以下の内容でお問い合わせを受け付けいたしました。\n確認させていただき、ご入力いただいたメールアドレスにご連絡いたしますので今しばらくお待ちくださいませ。\n\n--------------------\nName: {0}様\n\nEmail: {1}\n\nMessage:\n{2}\n--------------------'.format(name, email, message)
-    to_list = [email]
-    bcc_list = [settings.EMAIL_HOST_USER]
-
-    try:
-      message = EmailMessage(subject=subject, body=message, to=to_list, bcc=bcc_list)
-      message.send()
-    except BadHeaderError:
-      return HttpResponse("無効なヘッダが検出されました。")
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,25 +1,61 @@
 # from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import View
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
-
+from django.conf import settings
+from django.core.mail import BadHeaderError, EmailMessage
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from .forms import ContactForm
+import re
+
 
 class IndexView(TemplateView):
   template_name = 'portfolio/index.html'
 
-class ContactFormView(FormView):
-  template_name = 'portfolio/contact_form.html'
-  form_class = ContactForm
-  success_url = reverse_lazy('contact_thanks')
+class ContactFormView(View):
+  def get(self, request, *args, **kwargs):
+    form = ContactForm(request.POST or None)
 
-  def form_valid(self, form):
-    form.send_email()
-    return super().form_valid(form)
+    return render(request, 'portfolio/contact_form.html', {
+      'form': form
+    })
+
+  def post(self, request, *args, **kwargs):
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+      name = form.cleaned_data['name']
+      email = form.cleaned_data['email']
+      message = form.cleaned_data['message']
+
+      context = {
+        'name': name,
+        'email': email,
+        'message': message
+      }
+
+      subject = render_to_string('portfolio/mail_template/contact_subject.txt', context)
+      body = render_to_string('portfolio/mail_template/contact_message.txt', context)
+      to_list = [email]
+      bcc_list = [settings.EMAIL_HOST_USER]
+
+      try:
+          # ひらがなを含む場合のみメール送信
+          if re.search('[ぁ-ん]', message) != None:
+              message = EmailMessage(subject=subject, body=body, to=to_list, bcc=bcc_list)
+              message.send()
+          else:
+              return HttpResponse("メッセージ内容に問題が発生したため、送信がキャンセルされました。")
+      except BadHeaderError:
+          return HttpResponse("無効なヘッダが検出されました。")
+
+      return redirect('contact_thanks')
+
+    return render(request, 'portfolio/contact_form.html', {
+        'form': form
+    })
 
 class ContactThanksView(TemplateView):
   template_name = 'portfolio/contact_thanks.html'
-
-  # def get_context_data(self, **kwargs):
-  #   context = super().get_context_data(**kwargs)
-  #   context['success'] = "お問い合わせは正常に送信されました。"
