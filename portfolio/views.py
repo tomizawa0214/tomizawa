1e2bbeaf1f27cd3852563e4f7621c4364403445c
tomizawa0214 <j.tomizawa.japan@gmail.com>
2025-01-07 14:35:11 +0900

    edit Redirect

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,63 +3,23 @@ from django.views import View
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.core.mail import BadHeaderError, EmailMessage
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import render_to_string
 from .forms import ContactForm
 import re
 
 
-class IndexView(TemplateView):
-    template_name = 'portfolio/index.html'
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        redirect_url = 'https://www.alkunelma.co.jp'
+        return HttpResponseRedirect(redirect_url)
 
 class ContactFormView(View):
     def get(self, request, *args, **kwargs):
-        form = ContactForm(request.POST or None)
+        redirect_url = 'https://www.alkunelma.co.jp#contact'
+        return HttpResponseRedirect(redirect_url)
 
-        return render(request, 'portfolio/contact_form.html', {
-            'form': form
-        })
-
-    def post(self, request, *args, **kwargs):
-        form = ContactForm(request.POST or None)
-
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-
-            context = {
-                'name': name,
-                'email': email,
-                'message': message
-            }
-
-            subject = render_to_string('portfolio/mail_template/contact_subject.txt', context)
-            body = render_to_string('portfolio/mail_template/contact_message.txt', context)
-            to_list = [email]
-            bcc_list = [settings.EMAIL_HOST_USER]
-
-            try:
-                # ひらがなを含む場合のみメール送信
-                if re.search('[ぁ-ん]', message) != None:
-                    message = EmailMessage(
-                        subject=subject,
-                        body=body,
-                        to=to_list,
-                        bcc=bcc_list,
-                        from_email=settings.FROM_EMAIL,
-                    )
-                    message.send()
-                else:
-                    return HttpResponse("メッセージ内容に問題が発生したため、送信がキャンセルされました。")
-            except BadHeaderError:
-                return HttpResponse("無効なヘッダが検出されました。")
-
-            return redirect('contact_thanks')
-
-        return render(request, 'portfolio/contact_form.html', {
-            'form': form
-        })
-
-class ContactThanksView(TemplateView):
-    template_name = 'portfolio/contact_thanks.html'
+class ContactThanksView(View):
+    def get(self, request, *args, **kwargs):
+        redirect_url = 'https://www.alkunelma.co.jp'
+        return HttpResponseRedirect(redirect_url)
